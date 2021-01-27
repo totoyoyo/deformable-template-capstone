ffc6beaedfd7e17f7dab8f07590a354fbf5116b6
Peeranat (ToTo) Tokaeo <totoyoyorocks@gmail.com>
2021-01-27 23:00:04 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class yo(a,b,c):
+    def init(self,test1):
+        test1
+
+
+    def __init__(self):
+        5
+
+
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
