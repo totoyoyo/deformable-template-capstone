Fix missing minus sign

constants/constants_2d_0.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-TEMPLATE_SD2 = 2
-DEFORM_SD2 = 2
+TEMPLATE_SD2 = 1
+DEFORM_SD2 = 1
 SD_INIT = 1
 
 IMAGE_NROWS = 10
@@ -82,16 +82,16 @@ def gaussian_kernel_original(x_val, sd):
     return out
 
 
-def gaussian_kernel_given_diffs(diffs, sd):
+def gaussian_kernel_given_diffs(diffs, sd2):
     """
 
     :param diffs: a 2d array with each row denoting vector (calculated diff)
     :param sd: sd not squared
     :return: A 1d array of calculated gaussians
     """
-    norm2_squared =  np.sum((diffs ** 2),axis=1)
-    inter = (-((norm2_squared) ** 2)
-             / (2 * sd**2))
+    norm2_squared = np.sum((diffs ** 2),axis=1)
+    inter = (-((norm2_squared))
+             / (2 * sd2))
     out = np.exp(inter)
     # Should be a float
     return out

functions/functions_2d_fix.py
@@ -125,9 +125,28 @@ def get_pixel_by_centers_matrix(all_pixels, all_centers, sd2):
     reshaped_gauss = gaussian_out.reshape((n_pixels,n_centers))
     return reshaped_gauss
 
+def get_pixel_by_centers_matrix_mul_only(all_pixels, all_centers, sd2):
+    one_col2 = np.ones((2, 1))
+    n_pixels = np.shape(all_pixels)[0]
+    n_centers = np.shape(all_centers)[0]
+    p_norm_squared = (all_pixels ** 2) @ one_col2
+    c_norm_squared = (all_centers ** 2) @ one_col2
+    p_norm_squared_repeated = p_norm_squared @ np.ones((1, n_centers))
+    c_norm_squared_repeated = (c_norm_squared @ np.ones((1, n_pixels))).T
+    p_dot_c = 2 * (all_pixels @ all_centers.T)
+    big_matrix = p_norm_squared_repeated + c_norm_squared_repeated - p_dot_c
+    K = np.exp( - big_matrix / (2 * sd2))
+    return K
+
+
+
 PIXEL_G_CENTERS_MATRIX = get_pixel_by_centers_matrix(const.ALL_PIXELS,
                                                      const.G_CENTERS,
                                                      const.DEFORM_SD2)
+
+PIXEL_G_CENTERS_MATRIX_2 = get_pixel_by_centers_matrix_mul_only(const.ALL_PIXELS,
+                                                                const.G_CENTERS,
+                                                                const.DEFORM_SD2)
 
 
 def calculate_kBp(betas):
@@ -145,7 +164,7 @@ def timed_calculate_kBp(betas):
     deformation = PIXEL_G_CENTERS_MATRIX @ betas
     start = timer()
     deformed_pixel = const.ALL_PIXELS - deformation
-    out_matrix = get_pixel_by_centers_matrix(deformed_pixel,
+    out_matrix = get_pixel_by_centers_matrix_mul_only(deformed_pixel,
                                 const.P_CENTERS,
                                 const.TEMPLATE_SD2)
     end = timer()

helpers/solver_fix.py
@@ -1,5 +1,5 @@
 """
-Sample code automatically generated on 2021-02-27 15:39:40
+Sample code automatically generated on 2021-02-27 18:17:25
 
 by geno from www.geno-project.org
 
@@ -20,7 +20,7 @@ parameters
 variables
   matrix B
 min
-  1/2*tr(B'*Ginv*B)+1/(2*sdl2)*norm2(image-exp(((P-K*B).^2*oneCOL2*oneROWKp'+(Ca.^2*oneCOL2*oneROWL')'-2*(P-K*B)*Ca')/(2*sdp2))*A).^2
+  1/2*tr(B'*Ginv*B)+1/(2*sdl2)*norm2(image-exp(((P-K*B).^2*oneCOL2*oneROWKp'+(Ca.^2*oneCOL2*oneROWL')'-2*(P-K*B)*Ca')/((-2)*sdp2))*A).^2
 
 
 The generated code is provided "as is" without warranty of any kind.
@@ -47,11 +47,6 @@ except ImportError:
 
 # from scipy.optimize import minimize
 # USE_GENO_SOLVER = False
-# WRN = 'WARNING: GENO solver not installed. Using SciPy solver instead.\n' + \
-#       'Run:     pip install genosolver'
-# print('*' * 63)
-# print(WRN)
-# print('*' * 63)
 
 class GenoNLP:
     def __init__(self, Ginv, P, K, Ca, image, oneCOL2, oneROWKp, oneROWL, A, sdl2, sdp2):
@@ -148,11 +143,11 @@ class GenoNLP:
         B = self.variables(_x)
         T_0 = (self.P - (self.K).dot(B))
         t_1 = (1 / 2)
-        T_2 = np.exp(((1 / (2 * self.sdp2)) * ((np.multiply.outer(((T_0 ** 2)).dot(self.oneCOL2), self.oneROWKp) + np.multiply.outer(self.oneROWL, ((self.Ca ** 2)).dot(self.oneCOL2))) - (2 * (T_0).dot(self.Ca.T)))))
+        T_2 = np.exp(-((1 / (2 * self.sdp2)) * ((np.multiply.outer(((T_0 ** 2)).dot(self.oneCOL2), self.oneROWKp) + np.multiply.outer(self.oneROWL, ((self.Ca ** 2)).dot(self.oneCOL2))) - (2 * (T_0).dot(self.Ca.T)))))
         t_3 = (self.image - (T_2).dot(self.A))
         t_4 = (4 / ((4 * self.sdp2) * self.sdl2))
         f_ = ((np.trace(((B.T).dot(self.Ginv)).dot(B)) / 2) + ((np.linalg.norm(t_3) ** 2) / (2 * self.sdl2)))
-        g_0 = (((t_1 * (self.Ginv).dot(B)) + (t_1 * (self.Ginv.T).dot(B))) - ((t_4 * (((self.K.T).dot((t_3[:, np.newaxis] * T_2)) * self.A[np.newaxis, :])).dot(self.Ca)) - (t_4 * ((self.K.T).dot(((t_3 * (T_2).dot((self.A * self.oneROWKp)))[:, np.newaxis] * T_0)) * self.oneCOL2[np.newaxis, :]))))
+        g_0 = (((t_1 * (self.Ginv).dot(B)) + (t_1 * (self.Ginv.T).dot(B))) + ((t_4 * (((self.K.T).dot((t_3[:, np.newaxis] * T_2)) * self.A[np.newaxis, :])).dot(self.Ca)) - (t_4 * ((self.K.T).dot(((t_3 * (T_2).dot((self.A * self.oneROWKp)))[:, np.newaxis] * T_0)) * self.oneCOL2[np.newaxis, :]))))
         g_ = g_0.reshape(-1)
         return f_, g_
 